b6730ec06b1d01992b08e278e120d512492d795e
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-11-12 17:40:06 +0900

    Add Sample

---
+++ components/head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default = () => (
+  <div>
+    <Head>
+      <title>Hello Next</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import cow from 'cowsay-browser'
+import css from 'next/css'
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Hello Next</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
+    <main className={style}>
+      <h1><img id="log" className={logo} src="static/next.svg" title="NEXT.js" alt="NEXT.js"/></h1>
+      <div>Hello world!</div>
+      <pre className={pre}>{cow.say({text: 'Hi there!'})}</pre>
+    </main>
+  </div>
+)
+
+const pre = css({
+  fontFamily: 'Source Code Pro',
+  fontWeight: 100
+});
+
+const logo = css({
+  height: '30%',
+  width: '30%'
+});
+
+const style = css({
+  background: '#282A36',
+  color: 'white',
+  fontFamily: 'SFNSDisplay, "Hiragino Sans"',
+  fontWeight: 100
+});
